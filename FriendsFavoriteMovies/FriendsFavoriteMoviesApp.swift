863356a9060bd221ff96f48469fbbd8a6f54a541
Jeremy Kim <jedo@alumni.unc.edu>
2025-05-12 20:44:58 -0400

    Initial Commit

---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Jeremy Kim on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendsFavoriteMoviesTests/FriendsFavoriteMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesTests.swift
+//  FriendsFavoriteMoviesTests
+//
+//  Created by Jeremy Kim on 5/12/25.
+//
+
+import Testing
+@testable import FriendsFavoriteMovies
+
+struct FriendsFavoriteMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
